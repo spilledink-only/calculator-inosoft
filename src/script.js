5e71fce41bb82c7ebb268e802cf731b370b8d4f4
spilledink-only <155401603+spilledink-only@users.noreply.github.com>
2024-01-20 14:54:53 +0000

    finishing up

--- src/script.js
+++ src/script.js
@@ -11,13 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
       .replace("÷", "/")
       .replace('%', '*0.01')
       .replace('sin', 'Math.sin')
-      .replace('cos', 'Math.cos')
-      .replace('ln', 'Math.log')
-      .replace('π','Math.PI')
-      .replace('log','Math.log10')
-      .replace('e','Math.E')
-      .replace('tan','Math.tan')
-      .replace('√','Math.sqrt');
+      .replace('cos', 'Math.cos');
 
       console.log('convertedValue:', convertedValue)
       const result = eval(convertedValue);
@@ -29,25 +23,24 @@ document.addEventListener("DOMContentLoaded", function () {
       const button = buttons [i];
       button.addEventListener('click', function () {
         const value = button.innerText;
-      });
-    }
 
     try {
       if (value == "AC") {
         currentValue = "";
         display.value = currentValue;
-       } else if(value = "=") {
+      } else if (value === "=") {
         evaluateResult();
-       } else {
+      } else {
         currentValue += value;
         display.value = currentValue;
-       };
-      } catch (error) {
+      }
+  } catch (error) {
       console.error(error);
       currentValue = "ERROR";
       display.value = currentValue;
     
     }
-
+  });
+    }
 
 });
